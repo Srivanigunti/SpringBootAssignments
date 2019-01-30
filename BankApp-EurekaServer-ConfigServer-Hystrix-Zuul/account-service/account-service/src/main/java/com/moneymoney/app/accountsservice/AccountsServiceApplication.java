4ef98a8f0f1e85b625af6bd25ba7f994e4ca70f5
Gunti <srivani.gunti@capgemini.com>
2019-01-30 09:45:42 +0530

    Adds Bank App with Eureka server config server hystrix and Zuul

---
+++ BankApp-EurekaServer-ConfigServer-Hystrix-Zuul/account-service/account-service/src/main/java/com/moneymoney/app/accountsservice/AccountsServiceApplication.java
@@ -0,0 +1,33 @@
+package com.moneymoney.app.accountsservice;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import com.moneymoney.app.accountsservice.entity.CurrentAccount;
+import com.moneymoney.app.accountsservice.entity.SavingsAccount;
+import com.moneymoney.app.accountsservice.repository.AccountsRepository;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AccountsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsServiceApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner populateData(AccountsRepository accountsRepository) {
+		return (arg) -> {
+			accountsRepository.save(new SavingsAccount(101,"Srivani",10_000.00,true));
+			accountsRepository.save(new SavingsAccount(102,"Kunni",25_000.00,true));
+			accountsRepository.save(new SavingsAccount(103,"Venkat",35_000.00,false));
+			accountsRepository.save(new CurrentAccount(104,"Aruna",40_000.00,5_000.00));
+			accountsRepository.save(new CurrentAccount(105,"VarunSai",50_000.00,5_000.00));
+			accountsRepository.save(new CurrentAccount(106,"Mani",60_000.00,5_000.00));
+		};
+	}
+	
+}
---
+++ BankApp-EurekaServer-ConfigServer-Hystrix-Zuul/website/src/main/java/com/moneymoney/web/controller/BankAppController.java
@@ -0,0 +1,240 @@
+package com.moneymoney.web.controller;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.hateoas.Link;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.moneymoney.web.entity.CurrentDataSet;
+import com.moneymoney.web.entity.Transaction;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+@EnableCircuitBreaker
+@RefreshScope
+@Controller
+public class BankAppController {
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@RequestMapping("/")
+	public String form() {
+		return "index";
+	}
+	
+	@RequestMapping("/depositForm")
+	public String depositForm() {
+		return "DepositForm";
+	}
+	
+	@HystrixCommand(fallbackMethod="fallbackDeposit")
+	@RequestMapping("/deposit")
+	public String deposit(@ModelAttribute Transaction transaction,
+			Model model) {
+		restTemplate.postForEntity("http://springzuul/transaction-service/transactions/deposit", 
+				transaction, null);
+		model.addAttribute("message","Success!");
+		return "DepositForm";
+	}
+	
+	public String fallbackDeposit(@ModelAttribute Transaction transaction,
+			Model model) {
+		model.addAttribute("message","Service is under maintenance.. try after sometime..!!!!!");
+		return "DepositForm";
+	}
+
+	@RequestMapping("/withdrawForm")
+	public String withdrawForm() {
+		return "WithdrawForm";
+	}
+	
+	@HystrixCommand(fallbackMethod="fallbackWithdraw")
+	@RequestMapping("/withdraw")
+	public String withdraw(@ModelAttribute Transaction transaction,
+			Model model) {
+		restTemplate.postForEntity("http://springzuul/transaction-service/transactions/withdraw", 
+				transaction, null);
+		model.addAttribute("message","Success!");
+		return "WithdrawForm";
+	}
+	public String fallbackWithdraw(@ModelAttribute Transaction transaction,
+			Model model) {
+		model.addAttribute("message","Service is under maintenance.. try after sometime..!!!!!");
+		return "WithdrawForm";
+	}
+	
+	@RequestMapping("/fundTransferForm")
+	public String fundTransferForm() {
+		return "FundTransferForm";
+	}
+	
+	@HystrixCommand(fallbackMethod="fallbackFundTransfer")
+	@RequestMapping("/fundTransfer")
+	public String fundTransfer(@RequestParam("senderAccountNumber") int senderAccountNumber,
+			@RequestParam("receiverAccountNumber") int receiverAccountNumber,
+			@RequestParam("amount") double amount,@ModelAttribute Transaction transaction, Model model) {
+		transaction.setAccountNumber(senderAccountNumber);
+		restTemplate.postForEntity("http://springzuul/transaction-service/transactions/withdraw", transaction, null);
+		transaction.setAccountNumber(receiverAccountNumber);
+		restTemplate.postForEntity("http://springzuul/transaction-service/transactions/deposit", transaction, null);
+		model.addAttribute("message","Success!");
+		return "FundTransferForm";
+	}
+	
+	public String fallbackFundTransfer(@RequestParam("senderAccountNumber") int senderAccountNumber,
+			@RequestParam("receiverAccountNumber") int receiverAccountNumber,
+			@RequestParam("amount") double amount,@ModelAttribute Transaction transaction, Model model) {
+		model.addAttribute("message","Service is under maintenance.. try after sometime..!!!!!");
+		return "FundTransferForm";
+	}
+	
+	@RequestMapping("/statementDeposit")
+	@HystrixCommand(fallbackMethod="fallbackDepositStatement")
+	public ModelAndView getStatementDeposit(@RequestParam("offset") int offset, @RequestParam("size") int size) {
+		CurrentDataSet currentDataSet = restTemplate.getForObject("http://springzuul/transaction-service/transactions/statement", CurrentDataSet.class);
+		int currentSize=size==0?5:size;
+		int currentOffset=offset==0?1:offset;
+		Link next=linkTo(methodOn(BankAppController.class).getStatementDeposit(currentOffset+currentSize,currentSize)).withRel("next");
+		Link previous=linkTo(methodOn(BankAppController.class).getStatementDeposit(currentOffset-currentSize, currentSize)).withRel("previous");
+		List<Transaction> transactions = currentDataSet.getTransactions();
+		List<Transaction> currentDataSetList = new ArrayList<Transaction>();
+		
+		for (int i = currentOffset - 1; i < currentSize + currentOffset - 1; i++) { 
+			  if((transactions.size()<=i && i>0) || currentOffset<1) 
+				  break;
+			Transaction transaction = transactions.get(i);
+			currentDataSetList.add(transaction);
+			
+		}
+		CurrentDataSet dataSet = new CurrentDataSet(currentDataSetList, next, previous);
+		ModelAndView modelView = new ModelAndView();
+		modelView.addObject("currentDataSet",dataSet);
+		modelView.setViewName("DepositForm");
+		return modelView;
+	}
+	
+	public ModelAndView fallbackDepositStatement(@RequestParam("offset") int offset, @RequestParam("size") int size) {
+		int currentSize=size==0?5:size;
+		int currentOffset=offset==0?1:offset;
+		Link next=linkTo(methodOn(BankAppController.class).getStatementDeposit(currentOffset+currentSize,currentSize)).withRel("next");
+		Link previous=linkTo(methodOn(BankAppController.class).getStatementDeposit(currentOffset-currentSize, currentSize)).withRel("previous");
+		
+		List<Transaction> currentDataSetList = new ArrayList<Transaction>();
+		
+		for (int i = currentOffset - 1; i < currentSize + currentOffset - 1; i++) { 
+			  if(currentOffset<1) 
+				  break;
+			
+		}
+		CurrentDataSet dataSet = new CurrentDataSet(currentDataSetList, next, previous);
+		ModelAndView modelView = new ModelAndView();
+		modelView.addObject("currentDataSet",dataSet);
+		modelView.setViewName("DepositForm");
+		modelView.addObject("message","Service is under maintenance.. try after sometime..!!!!!");
+		return modelView;
+	}
+	
+	@HystrixCommand(fallbackMethod="fallbackWithdrawStatement")
+	@RequestMapping("/statementWithdraw")
+	public ModelAndView getStatementWithdraw(@RequestParam("offset") int offset, @RequestParam("size") int size) {
+		CurrentDataSet currentDataSet = restTemplate.getForObject("http://springzuul/transaction-service/transactions/statement", CurrentDataSet.class);
+		int currentSize=size==0?5:size;
+		int currentOffset=offset==0?1:offset;
+		Link next=linkTo(methodOn(BankAppController.class).getStatementWithdraw(currentOffset+currentSize,currentSize)).withRel("next");
+		Link previous=linkTo(methodOn(BankAppController.class).getStatementWithdraw(currentOffset-currentSize, currentSize)).withRel("previous");
+		List<Transaction> transactions = currentDataSet.getTransactions();
+		List<Transaction> currentDataSetList = new ArrayList<Transaction>();
+		
+		for (int i = currentOffset - 1; i < currentSize + currentOffset - 1; i++) { 
+			  if((transactions.size()<=i && i>0) || currentOffset<1) 
+				  break;
+			Transaction transaction = transactions.get(i);
+			currentDataSetList.add(transaction);
+			
+		}
+		CurrentDataSet dataSet = new CurrentDataSet(currentDataSetList, next, previous);
+		ModelAndView modelView = new ModelAndView();
+		modelView.addObject("currentDataSet",dataSet);
+		modelView.setViewName("WithdrawForm");
+		return modelView;
+	}
+	
+	public ModelAndView fallbackWithdrawStatement(@RequestParam("offset") int offset, @RequestParam("size") int size) {
+		int currentSize=size==0?5:size;
+		int currentOffset=offset==0?1:offset;
+		Link next=linkTo(methodOn(BankAppController.class).getStatementDeposit(currentOffset+currentSize,currentSize)).withRel("next");
+		Link previous=linkTo(methodOn(BankAppController.class).getStatementDeposit(currentOffset-currentSize, currentSize)).withRel("previous");
+		
+		List<Transaction> currentDataSetList = new ArrayList<Transaction>();
+		
+		for (int i = currentOffset - 1; i < currentSize + currentOffset - 1; i++) { 
+			  if(currentOffset<1) 
+				  break;
+			
+		}
+		CurrentDataSet dataSet = new CurrentDataSet(currentDataSetList, next, previous);
+		ModelAndView modelView = new ModelAndView();
+		modelView.addObject("currentDataSet",dataSet);
+		modelView.setViewName("WithdrawForm");
+		modelView.addObject("message","Service is under maintenance.. try after sometime..!!!!!!");
+		return modelView;
+	}
+	
+	@HystrixCommand(fallbackMethod="fallbackFundTransferStatement")
+	@RequestMapping("/statementFundTransfer")
+	public ModelAndView getStatementFundTransfer(@RequestParam("offset") int offset, @RequestParam("size") int size) {
+		CurrentDataSet currentDataSet = restTemplate.getForObject("http://springzuul/transaction-service/transactions/statement", CurrentDataSet.class);
+		int currentSize=size==0?5:size;
+		int currentOffset=offset==0?1:offset;
+		Link next=linkTo(methodOn(BankAppController.class).getStatementFundTransfer(currentOffset+currentSize,currentSize)).withRel("next");
+		Link previous=linkTo(methodOn(BankAppController.class).getStatementFundTransfer(currentOffset-currentSize, currentSize)).withRel("previous");
+		List<Transaction> transactions = currentDataSet.getTransactions();
+		List<Transaction> currentDataSetList = new ArrayList<Transaction>();
+		
+		for (int i = currentOffset - 1; i < currentSize + currentOffset - 1; i++) { 
+			  if((transactions.size()<=i && i>0) || currentOffset<1) 
+				  break;
+			Transaction transaction = transactions.get(i);
+			currentDataSetList.add(transaction);
+			
+		}
+		CurrentDataSet dataSet = new CurrentDataSet(currentDataSetList, next, previous);
+		ModelAndView modelView = new ModelAndView();
+		modelView.addObject("currentDataSet",dataSet);
+		modelView.setViewName("FundTransferForm");
+		return modelView;
+	}
+	public ModelAndView fallbackFundTransferStatement(@RequestParam("offset") int offset, @RequestParam("size") int size) {
+		int currentSize=size==0?5:size;
+		int currentOffset=offset==0?1:offset;
+		Link next=linkTo(methodOn(BankAppController.class).getStatementDeposit(currentOffset+currentSize,currentSize)).withRel("next");
+		Link previous=linkTo(methodOn(BankAppController.class).getStatementDeposit(currentOffset-currentSize, currentSize)).withRel("previous");
+		
+		List<Transaction> currentDataSetList = new ArrayList<Transaction>();
+		
+		for (int i = currentOffset - 1; i < currentSize + currentOffset - 1; i++) { 
+			  if(currentOffset<1) 
+				  break;
+			
+		}
+		CurrentDataSet dataSet = new CurrentDataSet(currentDataSetList, next, previous);
+		ModelAndView modelView = new ModelAndView();
+		modelView.addObject("currentDataSet",dataSet);
+		modelView.setViewName("FundTransferForm");
+		modelView.addObject("message","Service is under maintenance.. try after sometime..!!!!!");
+		return modelView;
+	}
+}
